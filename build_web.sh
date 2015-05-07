aeb25bb11d89dfd3c51274f48272b295e9688fb7
Sergio Gonzalez <bigmonachus@gmail.com>
2015-05-07 14:10:02 -0400

    [wip] sdl

--- build_web.sh
+++ build_web.sh
@@ -1,1 +1,1 @@
-emcc src/web_milton.c -o build/HTML/web_milton.html
+emcc src/web_milton.c -s USE_SDL=2 -o build/HTML/web_milton.html
--- src/web_milton.c
+++ src/web_milton.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
+#include <assert.h>
+#include <stdio.h>
+
 
 int main()
 {
-    printf("Hello, Emscripten!!!\n");
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_GLContext context;
+
+    assert (SDL_Init(SDL_INIT_VIDEO) == 0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    window = SDL_CreateWindow("Milton", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            640, 480, SDL_WINDOW_OPENGL);
+
+    int width = 1720;
+    int height = 940;
+    //SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_OPENGL, &window, &renderer);
+    assert (window);
     return 0;
 }
