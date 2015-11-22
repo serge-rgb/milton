cdfc607ad11e832670418c325eb5f498154fd218
Sergio Gonzalez <bigmonachus@gmail.com>
2015-11-22 00:17:27 -0600

    port imgui helper shader to v120

--- third_party/imgui/imgui_impl_sdl_gl3.cpp
+++ third_party/imgui/imgui_impl_sdl_gl3.cpp
@@ -6,6 +6,7 @@
 
 #include <imgui.h>
 #include "imgui_impl_sdl_gl3.h"
+#include "../../src/gl_helpers.h"
 
 
 // Data
@@ -188,13 +189,13 @@ bool ImGui_ImplSDLGL3_CreateDeviceObjects()
     glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
 
     const GLchar *vertex_shader =
-            "#version 330\n"
+            "#version 120\n"
             "uniform mat4 ProjMtx;\n"
-            "in vec2 Position;\n"
-            "in vec2 UV;\n"
-            "in vec4 Color;\n"
-            "out vec2 Frag_UV;\n"
-            "out vec4 Frag_Color;\n"
+            "attribute vec2 Position;\n"
+            "attribute vec2 UV;\n"
+            "attribute vec4 Color;\n"
+            "varying vec2 Frag_UV;\n"
+            "varying vec4 Frag_Color;\n"
             "void main()\n"
             "{\n"
             "	Frag_UV = UV;\n"
@@ -203,14 +204,15 @@ bool ImGui_ImplSDLGL3_CreateDeviceObjects()
             "}\n";
 
     const GLchar* fragment_shader =
-            "#version 330\n"
+            "#version 120\n"
             "uniform sampler2D Texture;\n"
-            "in vec2 Frag_UV;\n"
-            "in vec4 Frag_Color;\n"
-            "out vec4 Out_Color;\n"
+            "varying vec2 Frag_UV;\n"
+            "varying vec4 Frag_Color;\n"
+            //"out vec4 Out_Color;\n"
             "void main()\n"
             "{\n"
-            "	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
+            //"	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
+            "   gl_FragColor = Frag_Color * texture2D( Texture, Frag_UV.st);\n"
             "}\n";
 
     g_ShaderHandle = glCreateProgram();
