eaf7ba914351ddb1530a10207c65cb7df6446256
Sergio Gonzalez <bigmonachus@gmail.com>
2016-03-23 12:45:48 -0600

    switch to MIT license

--- src/platform_OSX_SDL_hooks.h
+++ src/platform_OSX_SDL_hooks.h
@@ -1,17 +1,5 @@
-// This file is part of Milton.
-//
-// Milton is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// Milton is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
-// more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Milton.  If not, see <http://www.gnu.org/licenses/>.
+// Copyright (c) 2015-2016 Sergio Gonzalez. All rights reserved.
+// License: https://github.com/serge-rgb/milton#license
 
 #pragma once
 
