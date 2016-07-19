c344a6a35bb617c78718c966cb8686f02ae9aa14
Sergio Gonzalez <bigmonachus@gmail.com>
2016-07-18 21:36:29 -0500

    fix wheel

--- src/picker.f.glsl
+++ src/picker.f.glsl
@@ -10,7 +10,7 @@ in vec2 v_norm;
 
 float radians_to_degrees(float r)
 {
-    return (180 * r) / PI;
+    return (180.0 * r) / PI;
 }
 
 // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
@@ -21,6 +21,61 @@ vec3 hsv2rgb(vec3 c)
     return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
+vec3 hsv_to_rgb(vec3 hsv)
+{
+    vec3 rgb;
+
+    float h = hsv.x;
+    float s = hsv.y;
+    float v = hsv.z;
+    float hh = h / 60.0;
+    int hi = int(floor(hh));
+    float cr = v * s;
+    float rem = mod(hh, 2.0);
+    float x = cr * (1.0 - abs(rem - 1.0));
+    float m = v - cr;
+
+    switch (hi)
+    {
+    case 0:
+        rgb.r = cr;
+        rgb.g = x;
+        rgb.b = 0;
+        break;
+    case 1:
+        rgb.r = x;
+        rgb.g = cr;
+        rgb.b = 0;
+        break;
+    case 2:
+        rgb.r = 0;
+        rgb.g = cr;
+        rgb.b = x;
+        break;
+    case 3:
+        rgb.r = 0;
+        rgb.g = x;
+        rgb.b = cr;
+        break;
+    case 4:
+        rgb.r = x;
+        rgb.g = 0;
+        rgb.b = cr;
+        break;
+    case 5:
+        rgb.r = cr;
+        rgb.g = 0;
+        rgb.b = x;
+        break;
+    default:
+        break;
+    }
+    rgb.r += m;
+    rgb.g += m;
+    rgb.b += m;
+
+    return rgb;
+}
 void main()
 {
     // NOTE:
@@ -38,16 +93,16 @@ void main()
     if (dist < radius+half_width &&
         dist > radius-half_width)
     {
-        vec2 n = v_norm / length(v_norm);
-        vec2 v = vec2(1/200.0,0);
-        float dotp = dot(n, v);
-        float angle = acos(dotp) / (length(n) * length(v));
-        /* if (v_norm.y > 0) */
-        /* { */
-        /*     angle = (2*PI) - angle; */
-        /* } */
+        vec2 n = v_norm;
+        vec2 v = vec2(1, 0);
+        float angle = acos(dot(n, v)/ (length(n) * length(v)));
+        if (v_norm.y > 0)
+        {
+            angle = (2*PI) - angle;
+        }
         vec3 hsv = vec3(radians_to_degrees(angle),1.0,1.0);
-        vec3 rgb = hsv2rgb(hsv);
+        //vec3 hsv = vec3(angle,1.0,1.0);
+        vec3 rgb = hsv_to_rgb(hsv);
         color = vec4(rgb,1);
     }
     gl_FragColor = color;
