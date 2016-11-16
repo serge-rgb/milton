feabe5a27f041c7eb937d4d198c21d2b8f833057
Mio Iwakura <mio.iwakura@gmail.com>
2016-11-16 16:10:48 -0600

    implement Linux open/save dialogs and persistence

--- src/platform_linux.cc
+++ src/platform_linux.cc
@@ -1,6 +1,8 @@
 #define IMPL_MISSING mlt_assert(!"IMPLEMENT")
 
 // #define _GNU_SOURCE //temporarily targeting gcc for program_invocation_name
+#include <sys/stat.h>
+#include <sys/time.h>
 #include <errno.h>
 #include <time.h>
 
@@ -51,6 +53,30 @@ b32 platform_delete_file_at_config(PATH_CHAR* fname, int error_tolerance)
     return result;
 }
 
+void linux_set_GTK_filter(GtkFileChooser* chooser, GtkFileFilter* filter, FileKind kind)
+{
+    switch(kind)
+    {
+    case FileKind_IMAGE:
+        {
+            gtk_file_filter_set_name(filter, "jpeg images (.jpg, .jpeg)");
+            gtk_file_filter_add_pattern(filter, "*.[jJ][pP][gG]");
+            gtk_file_filter_add_pattern(filter, "*.[jJ][pP][eE][gG]");
+            gtk_file_chooser_add_filter(chooser, filter);
+        } break;
+    case FileKind_MILTON_CANVAS:
+        {
+            gtk_file_filter_set_name(filter, "milton canvases (.mlt)");
+            gtk_file_filter_add_pattern(filter, "*.[mM][lL][tT]");
+            gtk_file_chooser_add_filter(chooser, filter);
+        } break;
+    default:
+        {
+        INVALID_CODE_PATH;
+    }
+    }
+}
+
 void platform_dialog(char* info, char* title)
 {
     IMPL_MISSING;
@@ -64,11 +90,23 @@ b32 platform_dialog_yesno(char* info, char* title)
 
 void platform_fname_at_config(PATH_CHAR* fname, size_t len)
 {
-    char* string_copy = (char*)mlt_calloc(1, len);
+    char *string_copy = (char*)mlt_calloc(1, len);
     if (string_copy)
     {
         strncpy(string_copy, fname, len);
-        snprintf(fname, len,  "~/.milton/%s", string_copy);
+        char *folder;
+        char *home = getenv("XDG_DATA_HOME");
+        if(!home)
+        {
+            home = getenv("HOME");
+            folder = ".milton";
+        }
+        else
+            folder = "milton";
+        snprintf(fname, len, "%s/%s", home, folder);
+        mkdir(fname, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+        strncat(fname, "/", len);
+        strncat(fname, string_copy, len);
         mlt_free(string_copy);
     }
 }
@@ -112,8 +150,30 @@ b32 platform_move_file(PATH_CHAR* src, PATH_CHAR* dest)
 
 PATH_CHAR* platform_open_dialog(FileKind kind)
 {
-    IMPL_MISSING;
-    return NULL;
+    cursor_show();
+    GtkWidget *dialog = gtk_file_chooser_dialog_new(
+            "Open File",
+            NULL,
+            GTK_FILE_CHOOSER_ACTION_OPEN,
+            "Cancel", GTK_RESPONSE_CANCEL,
+            "Open", GTK_RESPONSE_ACCEPT,
+            NULL
+            );
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+    GtkFileFilter *filter = gtk_file_filter_new();
+    linux_set_GTK_filter(chooser, filter, kind);
+    gtk_file_chooser_set_current_folder(chooser, getenv("HOME"));
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_ACCEPT)
+    {
+        gtk_widget_destroy(dialog);
+        return NULL;
+    }
+    gchar *gtk_filename = gtk_file_chooser_get_filename(chooser);
+    PATH_CHAR* open_filename = (PATH_CHAR*)mlt_calloc(MAX_PATH, sizeof(PATH_CHAR));
+    PATH_STRNCPY(open_filename, gtk_filename, MAX_PATH);
+    g_free(gtk_filename);
+    gtk_widget_destroy(dialog);
+    return open_filename;
 }
 void platform_open_link(char* link)
 {
@@ -122,14 +182,45 @@ void platform_open_link(char* link)
 }
 PATH_CHAR* platform_save_dialog(FileKind kind)
 {
-    IMPL_MISSING;
-    return NULL;
+    cursor_show();
+    GtkWidget *dialog = gtk_file_chooser_dialog_new(
+            "Save File",
+            NULL,
+            GTK_FILE_CHOOSER_ACTION_SAVE,
+            "Cancel", GTK_RESPONSE_CANCEL,
+            "Save", GTK_RESPONSE_ACCEPT,
+            NULL
+            );
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+    GtkFileFilter *filter = gtk_file_filter_new();
+    linux_set_GTK_filter(chooser, filter, kind);
+    gtk_file_chooser_set_do_overwrite_confirmation(chooser, TRUE);
+    gtk_file_chooser_set_current_folder(chooser, getenv("HOME"));
+    gtk_file_chooser_set_current_name(chooser, kind == FileKind_IMAGE ? "untitled.jpg" : "untitled.mlt");
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_ACCEPT)
+    {
+        gtk_widget_destroy(dialog);
+        return NULL;
+    }
+    gchar *gtk_filename = gtk_file_chooser_get_filename(chooser);
+    PATH_CHAR* save_filename = (PATH_CHAR*)mlt_calloc(MAX_PATH, sizeof(PATH_CHAR));
+    PATH_STRNCPY(save_filename, gtk_filename, MAX_PATH);
+    g_free(gtk_filename);
+    gtk_widget_destroy(dialog);
+    return save_filename;
 }
 //  ====
 
 WallTime platform_get_walltime()
 {
     WallTime wt = {0};
-    IMPL_MISSING;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    struct tm *time;
+    time = localtime(&tv.tv_sec);
+    wt.h = time->tm_hour;
+    wt.m = time->tm_min;
+    wt.s = time->tm_sec;
+    wt.ms = tv.tv_usec / 1000;
     return wt;
 }
