8d95e205c4d7fbc777ced63092a76b2194f9ba64
Sergio Gonzalez <bigmonachus@gmail.com>
2018-06-03 11:03:37 -0700

    Script for building SDL. Update library binaries.

---
+++ third_party/build_deps.bat
@@ -0,0 +1,50 @@
+@echo off
+
+
+:: NOTE: Milton provides built SDL binaries for Windows 64 and 32 bit. If for
+:: whatever reason you need to go through the hassle of building SDL, follow
+:: these instructions.
+::
+::
+
+:: Windows SDK 15063
+
+:: The provided is vanilla SDL-2.0.8 with the following changes:
+::
+:: Directories removed to save space:
+::    ./test
+::    XCode iOS Demo dir
+::
+:: VS SDL project changed to compile a static library instead of a DLL.
+::
+:: Building:
+::   Either run this script from the third_party directory. Or do the following by hand:
+::
+:: In the SDL2-2.0.8 directory, open VisualC\SDL.sln in Visual Studio 2017 and
+:: build the SDL project. Do not build the whole solution, as the test
+:: directory has been deleted.
+::
+:: In a VS command prompt, this script attempts to do the above in an automated
+:: fashion.
+::
+
+set target=x64
+if "%1"=="x86" set target=x86
+
+
+if "%target%"=="x86" set outdir=Win32
+if "%target%"=="x64" set outdir=x64
+
+echo "Milton build_deps.bat: Building target platform %outdir%"
+
+pushd SDL2-2.0.8\VisualC
+msbuild SDL\SDL.vcxproj /p:Configuration="Debug" /p:Platform=%outdir%
+popd
+
+echo Copying SDL2 lib and pdb files to bin\%target%\
+copy SDL2-2.0.8\VisualC\SDL\%outdir%\Debug\SDL2.lib bin\%target%\SDL2.lib
+
+:: NOTE: For some reason the SDL project does not generate a PDB for 32 bit.
+:: Doesn't matter too much since dev is done in x64.
+if "%target%"=="x64" copy SDL2-2.0.8\VisualC\SDL\%outdir%\Debug\SDL2.pdb bin\%target%\SDL2.pdb
+
