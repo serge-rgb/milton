413849fac4645894be89237ea274458cdb707ea7
Sergio Gonzalez <bigmonachus@gmail.com>
2019-10-23 20:14:10 -0700

    Remove scripts directory

--- scripts/vcvars.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-rem "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
--- scripts/vs_build.bat
+++
@@ -1,2 +0,0 @@
-::devenv VS2015\MiltonPaint.sln /Build "Release|x64"
-msbuild VS2015\MiltonPaint.sln /nologo /v:m
